e28f029ddfda3cb8bd3d056b27f1afebbe7ae708
Hadar7 <hadar17898@gmail.com>
2022-09-12 11:16:50 +0300

    Reorder navbar (#11)
    * - Show Reservations
    
    - Need to fix header show in signin\signup
    
    * - Added css and Room.jsx
    
    * - Added show tasks and assignments in Tasks.js
    
    * -fix everything
    
    * - Tasks show only if there are available tasks.
    - Chart in Settings is now more readable
    - Alert fix - not to show twice
    - Deleted Layout.js
    
    * - Assignment shows on task finish
    - Default settings

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -1,93 +1,103 @@
-import {Card, CardContent, Modal, Typography,} from '@mui/material';
-import React, {useEffect, useState} from 'react';
-import {useNavigate, useParams} from 'react-router-dom';
+import { Card, CardContent, Modal, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import styles from './Rooms.module.css';
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TableBody from "@mui/material/TableBody";
-import TableContainer from "@mui/material/TableContainer";
-import "./StyleTable";
-import {customColumnStyle, customTableStyle, StyledTableCell, StyledTableRow} from "./StyleTable";
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import './StyleTable';
+import {
+  customColumnStyle,
+  customTableStyle,
+  StyledTableCell,
+  StyledTableRow,
+} from './StyleTable';
 
 const Assignment = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(true);
-    const [assignment, setAssignment] = useState(null);
-    const {taskId} = useParams();
-    const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [assignment, setAssignment] = useState(null);
+  const { taskId } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-        fetch(`/assignments/get_assignment/${taskId}`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((assignment) => {
-                setAssignment(assignment);
-            });
-    }, []);
+  useEffect(() => {
+    fetch(`/assignments/get_assignment/${taskId}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((assignment) => {
+        setAssignment(assignment);
+      });
+  }, []);
 
-    if (!assignment) return;
+  if (!assignment) return;
 
-    const closeModalHandler = () => {
-        setModalIsOpen(false);
-        navigate('/tasks');
-    };
-    return (
-        <div key={taskId}>
-            <Modal
-                className={styles.modal}
-                open={modalIsOpen}
-                onClose={closeModalHandler}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Card className={styles.card} sx={{padding: '1em'}}>
-                    <Typography
-                        sx={{textAlign: 'center'}}
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        {/* {room.roomNumber} */}
-                        The Assignment
-                    </Typography>
+  const closeModalHandler = () => {
+    setModalIsOpen(false);
+    navigate(location.state.basePath);
+  };
+  return (
+    <Modal
+      className={styles.modal}
+      open={modalIsOpen}
+      onClose={closeModalHandler}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Card className={styles.card} sx={{ padding: '1em' }}>
+        <Typography
+          sx={{ textAlign: 'center' }}
+          id="modal-modal-title"
+          variant="h6"
+          component="h2"
+        >
+          {/* {room.roomNumber} */}
+          The Assignment
+        </Typography>
 
-                    <CardContent id="modal-modal-description" sx={{height: "50%", mt: 2}}>
-                        <TableContainer component={Paper}>
-                            <Table style={customTableStyle} aria-label="customized table">
-                                <TableHead>
-                                    <TableRow>
-                                        <StyledTableCell style={customColumnStyle} align="left">
-                                            Reservation Number
-                                        </StyledTableCell>
-                                        <StyledTableCell style={customColumnStyle} align="left">
-                                            Room Number
-                                        </StyledTableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {Object.keys(assignment.reservationRoomMap).map((key, i) =>
-                                        (
-                                            <StyledTableRow key={key}>
-                                                <StyledTableCell component="th" scope="row"
-                                                                 style={{textDecoration: 'none'}}>
-                                                    {key}
-                                                </StyledTableCell>
-                                                <StyledTableCell component="th" scope="row"
-                                                                 style={{textDecoration: 'none'}}>
-                                                    {Object.values(assignment.reservationRoomMap)[i]}
-                                                </StyledTableCell>
-                                            </StyledTableRow>
-                                        )
-                                    )}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </CardContent>
-                </Card>
-            </Modal>
-        </div>
-    );
+        <CardContent id="modal-modal-description" sx={{ height: '50%', mt: 2 }}>
+          <TableContainer component={Paper}>
+            <Table style={customTableStyle} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell style={customColumnStyle} align="left">
+                    Reservation Number
+                  </StyledTableCell>
+                  <StyledTableCell style={customColumnStyle} align="left">
+                    Room Number
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Object.keys(assignment.reservationRoomMap).map(
+                  (reservationNum, i) => (
+                    <StyledTableRow key={reservationNum}>
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        style={{ textDecoration: 'none' }}
+                      >
+                        {reservationNum}
+                      </StyledTableCell>
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        style={{ textDecoration: 'none' }}
+                      >
+                        {Object.values(assignment.reservationRoomMap)[i]}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  )
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </CardContent>
+      </Card>
+    </Modal>
+  );
 };
 export default Assignment;
